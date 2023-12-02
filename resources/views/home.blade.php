a079532ffc36c4e4169d0f9d8de3e8f2441b4d7a
Awdrey Candido <awdrey.dev@gmail.com>
2023-12-02 17:40:57 -0300

    fix: fixing no contacts message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,57 +44,59 @@
     <main class="w-full lg:w-[45%] lg:min-w-[45%] p-4">
 
         @auth
-            <h1 class="text-2xl font-bold mb-6">
+            <h1 class="text-2xl font-bold mb-6 mt-4">
                 Your Contacts List
             </h1>
         @else
-            <h1 class="text-2xl font-bold mb-6">
+            <h1 class="text-2xl font-bold mb-6 mt-4">
                 Contacts List
             </h1>
         @endauth
 
-
         <div class=" flex flex-col gap-2">
 
-            <div class="flex text-lg font-medium gap-4 pl-4 items-center">
-                <div class="w-[150px]">Name</div>
-                <div class="w-[150px]">Phone Number</div>
-                <div class="w-[150px]">E-mail</div>
-            </div>
-            @foreach ($contacts as $contact)
-                <a class="cursor-pointer" href="/contact-details/{{ $contact->id }}">
-                    <div class="flex justify-between bg-[] px-4 py-2 border-2 border-gray-400 rounded-lg ">
-                        <div class="flex text-lg gap-4 items-center">
-                            <div class="w-[150px]">{{ $contact['name'] }}</div>
-                            <div class="w-[150px]">{{ $contact['contact'] }}</div>
-                            <div class="w-[150px]">{{ $contact['email'] }}</div>
-                        </div>
+            <hr class="my-4">
 
-                        @auth
-
-                            <div class="flex gap-4">
-                                <p
-                                    class="px-3 py-1 bg-blue-200 hover:bg-blue-500 hover:text-[#f2f2f2] rounded-lg duration-300">
-                                    <a href="/edit-contact/{{ $contact->id }}">Edit</a>
-                                </p>
-
-                                <form action="/delete-contact/{{ $contact->id }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button
-                                        class="px-3 py-1 bg-red-200 hover:bg-red-500 hover:text-[#f2f2f2] rounded-lg duration-300">Delete</button>
-                                </form>
+            @if (count($contacts) > 0)
+                <div class="flex text-lg font-medium gap-4 pl-4 items-center">
+                    <div class="w-[150px]">Name</div>
+                    <div class="w-[150px]">E-mail</div>
+                    <div class="w-[150px]">Phone Number</div>
+                </div>
+                @foreach ($contacts as $contact)
+                    <a class="cursor-pointer" href="/contact-details/{{ $contact->id }}">
+                        <div class="flex justify-between bg-[] px-4 py-2 border-2 border-gray-400 rounded-lg ">
+                            <div class="flex text-lg gap-4 items-center">
+                                <div class="w-[150px]">{{ $contact['name'] }}</div>
+                                <div class="w-[150px]">{{ $contact['email'] }}</div>
+                                <div class="w-[150px]">{{ $contact['contact'] }}</div>
                             </div>
-                        @endauth
-                    </div>
-                </a>
-            @endforeach
+                            @auth
+                                <div class="flex gap-4">
+                                    <p
+                                        class="px-3 py-1 bg-blue-200 hover:bg-blue-500 hover:text-[#f2f2f2] rounded-lg duration-300">
+                                        <a href="/edit-contact/{{ $contact->id }}">Edit</a>
+                                    </p>
+
+                                    <form action="/delete-contact/{{ $contact->id }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button
+                                            class="px-3 py-1 bg-red-200 hover:bg-red-500 hover:text-[#f2f2f2] rounded-lg duration-300">Delete</button>
+                                    </form>
+                                </div>
+                            @endauth
+                        </div>
+                    </a>
+                @endforeach
+            @else
+                <p class="text-lg text-gray-500 mt-4">There are not registered contacts.</p>
+            @endif
+
         </div>
 
-
-        {{--  LIST OF EXISTING CONTACTS  --}}
-
     </main>
+
 
 
 </body>
